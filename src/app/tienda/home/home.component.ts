ec245d9ffd33652afec94b64a36001b54b9d4078
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-08-24 15:27:44 -0500

    Agregando informacion en la seccion de about

--- src/app/tienda/home/home.component.ts
+++ src/app/tienda/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { HttpDataService } from '../service/http-data.service';
 
 @Component({
@@ -19,5 +20,25 @@ export class HomeComponent implements OnInit {
         console.log(data);
         this.productos = data
       });
+  }
+
+  confirmarCompra(): void {
+    Swal.fire({
+      title: 'Confirmar?',
+      text: "Estas usted seguro que quiere realizar la compra?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, estoy seguro'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Felicitaciones!',
+          'Su oren a sido realizada.',
+          'success'
+        )
+      }
+    })
   }
 }
